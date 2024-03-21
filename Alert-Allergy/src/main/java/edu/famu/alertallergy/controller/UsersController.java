535a4813d5f20dcb017b97c118c386f01c68ceb9
RobertBeautelus <103546272+Rbeautelus1@users.noreply.github.com>
2024-03-21 15:39:15 -0400

    Update UsersController.java

--- Alert-Allergy/src/main/java/edu/famu/alertallergy/controller/UsersController.java
+++ Alert-Allergy/src/main/java/edu/famu/alertallergy/controller/UsersController.java
@@ -1,97 +1,100 @@
 package edu.famu.alertallergy.controller;
-
-import com.google.cloud.firestore.DocumentReference;
 import edu.famu.alertallergy.models.Users.Users;
 import edu.famu.alertallergy.service.UsersService;
-import edu.famu.alertallergy.util.ErrorMessage;
-import edu.famu.alertallergy.util.ResponseWrapper;
-import org.springframework.beans.factory.annotation.Value;
+import edu.famu.alertallergy.util.ApiResponseFormat;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/user")
 public class UsersController {
 
-    UsersService usersService;
-    @Value("${response.status}")
-    private int statusCode;
-    @Value("${response.name}")
-    private String name;
-    private Object payload;
-    private ResponseWrapper response;
-    private static final String CLASS_NAME = "UserService";
+    private final UsersService usersService;
 
     public UsersController(UsersService usersService) {
         this.usersService = usersService;
-        payload = null;
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<Map<String, Object>> getAllUsers(){
-        try{
-            payload = usersService.getUsers();
-            statusCode = 200;
-            name = "users";
-        } catch (ExecutionException | InterruptedException e) {
-            payload = new ErrorMessage("Cannot fetch users from database", CLASS_NAME, e.toString());
-        }
-
-        response = new ResponseWrapper(statusCode,name, payload);
-
-        return response.getResponse();
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<Map<String,Object>> getUsers(DocumentReference id){
-        try{
-            payload = usersService.getUser(id);
-            statusCode = 200;
-            name = "user";
-        } catch (ExecutionException | InterruptedException e) {
-            payload = new ErrorMessage("Cannot fetch user with id " + id + " from database.",CLASS_NAME, e.toString());
-        }
-        response = new ResponseWrapper(statusCode,name, payload);
-
-        return response.getResponse();
     }
 
     @PostMapping("/")
-    public ResponseEntity<Map<String,Object>> createUser(@RequestBody Users users){
-        try{
-            payload = usersService.createUsers(users);
-            statusCode = 201;
-            name = "userId";
-        } catch (ExecutionException | InterruptedException e) {
-            payload = new ErrorMessage("Cannot create new user in database.", CLASS_NAME, e.toString());
+    public ResponseEntity<ApiResponseFormat<Users>> createUser(@RequestBody Users user) {
+        try {
+            Users createdUser = usersService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ApiResponseFormat<>(true, "Users successfully created.", createdUser,null));
+        } catch (Exception  e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseFormat<>(false, "Error creating user.", null,e.getMessage()));
         }
+    }
 
-        response = new ResponseWrapper(statusCode,name, payload);
+    @GetMapping("/")
+    public ResponseEntity<ApiResponseFormat<List<Users>>> getAllUsers() {
+        try {
+            UsersService usersService = new UsersService();
+            List<Users> userList = usersService.getAllUsers();
+            return ResponseEntity.ok(new ApiResponseFormat<>(true, "Users retrieved successfully", userList,null));
+        } catch (ExecutionException | InterruptedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseFormat<>(false, "Error retrieving users", null,e));
+        }
+    }
 
-        return response.getResponse();
+    @Operation(summary = "Get a user by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users found."),
+            @ApiResponse(responseCode = "404", description = "Users not found."),
+            @ApiResponse(responseCode = "500", description = "Unable to retrieve user.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponse.class))),
+    })
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponseFormat<Users>> getUserById(@PathVariable(name = "id") String userId) {
+        try {
+            Users user = usersService.getUserById(userId);
+            if (user != null) {
+                return ResponseEntity.ok(new ApiResponseFormat<>(true, "Users retrieved successfully", user,null));
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ApiResponseFormat<>(false, "Users not found", null,null));
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseFormat<>(false, "Error retrieving user", null,e));
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponseFormat<String>> deleteUser(@PathVariable String userId)
+    {
+        try {
+            usersService.deleteUser(userId);
+            return ResponseEntity.ok(new ApiResponseFormat<>(true, "Users deleted successfuly", null, null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseFormat<>(false,"Error deleting Users", null,e.getMessage()));
+        }
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<Map<String,Object>> updateUser(@PathVariable(name="userId") String id, @RequestBody Map<String, String> updateValues){
-
-
-        try{
-
-            usersService.updateUsers(id, updateValues);
-            statusCode = 201;
-            name = "message";
-            payload = "Update successful for user with id " + id;
-
-        } catch (Exception e) {
-            payload = new ErrorMessage("Cannot update user with id " + id,CLASS_NAME, e.toString());
+    public ResponseEntity<ApiResponseFormat<Users>> updateUsers(@PathVariable String userId, @RequestBody Users updateValues) {
+        try {
+           Users users = usersService.updateUsers(userId, updateValues);
+            return ResponseEntity.ok(new ApiResponseFormat<>( true, "User updated successfully",users,null));
+        } catch (ExecutionException | InterruptedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponseFormat<>(false, "Error updating users", null,e));
         }
-
-        response = new ResponseWrapper(statusCode,name, payload);
-
-        return response.getResponse();
     }
+
 
 }
